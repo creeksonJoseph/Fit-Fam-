742eb9e1b94dac5ed5cf00305986c34f8a100d88
ogzacky75 <ogzacky75@gmail.com>
2025-09-24 09:58:39 +0300

    adding changes to app in server side

--- Server/app.py
+++ Server/app.py
@@ -4,22 +4,33 @@ from flask_restful import Api,Resource
 from models import db
 from routes import User_routes, Workout_routes, Progress_routes, Friends_routes
 
-app = Flask(__name__)
+def create_app():
+    app = Flask(__name__)
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///fitness_app.db'
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+    app.config['SECRET_KEY'] = 'your_secret_key'
+    
+    db.init_app(app)
+    CORS(app)
+    api = Api(app)
 
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///fitness_app.db'
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-app.config['SECRET_KEY'] = 'secret_key'
-app.config['SESSION_TYPE'] = 'filesystem'
+    with app.app_context():
+        db.create_all()
+
+    api.add_resource(User_routes.UserRegister, '/register')
+    api.add_resource(User_routes.UserLogin, '/login')
+    api.add_resource(User_routes.UserProfile, '/profile/<int:user_id>')
+    
+    api.add_resource(Workout_routes.WorkoutList, '/workouts')
+    api.add_resource(Workout_routes.WorkoutDetail, '/workouts/<int:workout_id>')
+    
+    api.add_resource(Progress_routes.ProgressList, '/progress/<int:user_id>')
+    api.add_resource(Progress_routes.ProgressDetail, '/progress/<int:user_id>/<int:progress_id>')
+    
+    api.add_resource(Friends_routes.FriendList, '/friends/<int:user_id>')
+    api.add_resource(Friends_routes.FriendRequest, '/friends/request')
+    
+    return app
 
 
-CORS(app)
-api = Api(app)
-db.init_app(app)
-app.register_blueprint(User_routes, url_prefix='/users')
-app.register_blueprint(Workout_routes, url_prefix='/workouts')
-app.register_blueprint(Progress_routes, url_prefix='/progress')
-app.register_blueprint(Friends_routes, url_prefix='/friends')
-
-
-if __name__ == '__main__':
-    app.run(debug=True)
+app = create_app()
