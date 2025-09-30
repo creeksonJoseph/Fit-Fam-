da2127ecd5f2e3ed0660cab33b047e241ed4af1f
creeksonjoseph <charanajoseph@gmail.com>
2025-09-30 12:33:20 +0300

    updated cors to serve new frontend link

--- Server/app.py
+++ Server/app.py
@@ -39,8 +39,6 @@ def create_app():
          origins=[
              "http://localhost:3000",
              "http://127.0.0.1:3000",
-             'https://fit-fam.onrender.com',
-             'https://group-fitness-app.onrender.com',
              'https://fit-fam-six.vercel.app'], 
          supports_credentials=True, 
          allow_headers=['Content-Type', 'Authorization'],
